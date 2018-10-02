4cdae031790e673ac3614cd091bddd294b9b4cc9
Anika <Anika@Computer.local>
2018-10-01 23:46:26 -0400

    test

--- untitled1.py
+++ untitled1.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    user = {'username': 'Miguel'}
+    return render_template('base.html', title='Home', user=user)
 
 
 if __name__ == '__main__':
